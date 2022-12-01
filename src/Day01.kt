1f5fc997636d77dd28eea077f61b3e7ece451242
Matheus Fernando Finatti <matheus.finatti@superawesome.com>
2022-12-01 17:19:29 +0000

    day01 shorter code for fun

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,26 +1,7 @@
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlin.math.max
-
 fun main() {
     val input = readInput("Day01").trim().split("\n\n")
-
-    val elves = groupElves(input).sumElvesSnacks()
-    println(part01(elves))
-    println(part02(elves))
+    input.map { s -> s.split("\n").map(String::toInt) }
+        .fold(emptyList<Int>()) { elves, elf -> elves + listOf(elf.sum()) }
+        .also { elves -> println(elves.max()) } // part01
+        .also { elves -> println(elves.sortedDescending().take(3).sum()) } // part02
 }
-
-fun groupElves(input: List<String>) =
-    input.map { s ->
-        s.split("\n").map(String::toInt)
-    }
-
-fun List<List<Int>>.sumElvesSnacks() =
-    fold(listOf<Int>()) { elves, elf ->
-        elves + listOf(elf.sum())
-    }
-
-fun part01(elves: List<Int>) = elves.max()
-
-fun part02(elves: List<Int>) =
-    elves.sortedDescending().take(3).sum()
